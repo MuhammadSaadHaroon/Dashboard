94e9e8b7086f5a6e1ecbb0aba79a2742a74ab6ad
MuhammadSaadHaroon <saadharoon6800@gmail.com>
2025-10-18 11:34:30 +0500

    changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,8 +9,15 @@ const Dashboard = () => {
   return (
     <Box className="dashboard-container">
       <Typography variant="h4" className="dashboard-title">Welcome to the Dashboard</Typography>
+        <br></br>
       <Typography variant="body1" className="dashboard-text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate non aperiam ut eius commodi autem quos corrupti quae a ducimus cupiditate provident illo sed totam aliquam accusamus architecto, eum rerum.<br></br>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, officiis! Obcaecati eum pariatur odit. Facere pariatur totam quis doloremque mollitia dignissimos, nam consequuntur itaque neque eius provident, voluptas enim necessitatibus.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate non aperiam ut eius commodi autem quos corrupti quae a ducimus cupiditate provident illo sed totam aliquam accusamus architecto, eum rerum.
+        <br></br>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, officiis! Obcaecati eum pariatur odit. Facere pariatur totam quis doloremque mollitia dignissimos, nam consequuntur itaque neque eius provident, voluptas enim necessitatibus.
+        <br></br>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, officiis! Obcaecati eum pariatur odit. Facere pariatur totam quis doloremque mollitia dignissimos, nam consequuntur itaque neque eius provident, voluptas enim necessitatibus.
+        <br></br>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, officiis! Obcaecati eum pariatur odit. Facere pariatur totam quis doloremque mollitia dignissimos, nam consequuntur itaque neque eius provident, voluptas enim necessitatibus.
       </Typography>
     </Box>
   );
